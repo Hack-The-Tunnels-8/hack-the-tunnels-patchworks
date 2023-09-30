5f1bf383edcdfd135099114accc28d44639a8f79
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2023-09-30 20:29:23 +0000

    search bar done!

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -11,6 +11,7 @@ function Navbar() {
     <div className="navbar">
       <div className="navbar_wrapper">
       <div className="navbar__logo">
+      <div className="search-container">
 
         <Link to="/">
         <img src="https://cdn.glitch.global/def88561-e477-44d1-8fee-d2262d06e400/Component%201.svg?v=1696096848006"></img>
@@ -20,13 +21,18 @@ function Navbar() {
       <div className="navbar__account">
         {loggedIn() === false ? (
           <>
+            <input type="text" id="search-input" placeholder="Search..." />
+            <button type="button" id="search-button">Search</button>
             <button onClick={() => navigate("/sign-up")}>Sign Up</button>
             <button onClick={() => navigate("/login")}>Login</button>
-            <button onClick={() => navigate("/login")} id ="logout_btn">Logout</button>
+            <button onClick={() => navigate("/login")} id ="logout_btn">Logout</button> 
           </>
         ) : (
           <button onClick={() => logout()}>Logout</button>
-        )}
+        )
+        }
+            
+        </div>
       </div>
       </div>
     </div>
