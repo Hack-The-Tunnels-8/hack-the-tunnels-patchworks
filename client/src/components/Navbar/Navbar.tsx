123ed46f6eb4e4a9c463b75fba11e0ea1223e1f4
kristian:) <68247804+kristian-chaves@users.noreply.github.com>
2023-09-30 18:50:01 +0000

    NAVBAR MAVBARMAVBARMAVABAR
    Co-authored-by: JanKhoe <JanKhoe@users.noreply.github.com>
    Co-authored-by: littlemanstann <littlemanstann@users.noreply.github.com>
    Co-authored-by: Charelle Constantino <yeetyah1030@users.noreply.github.com>

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -9,18 +9,25 @@ function Navbar() {
 
   return (
     <div className="navbar">
+      <div className="navbar_wrapper">
       <div className="navbar__logo">
-        <Link to="/">Store Logo</Link>
+
+        <Link to="#">
+        <img src="https://cdn.glitch.global/def88561-e477-44d1-8fee-d2262d06e400/Component%201.svg?v=1696096848006"></img>
+        </Link>
+
       </div>
       <div className="navbar__account">
         {loggedIn() === false ? (
           <>
             <button onClick={() => navigate("/sign-up")}>Sign Up</button>
             <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/login")} id ="logout_btn">Logout</button>
           </>
         ) : (
           <button onClick={() => logout()}>Logout</button>
         )}
+      </div>
       </div>
     </div>
   );
